f6d1e690760513b8164506b517060a7c86273ef2
zylimz107 <zylimz@hotmail.com>
2024-11-08 17:10:16 +0800

    Add updated files

--- used_car.py
+++ used_car.py
@@ -25,3 +25,15 @@ class UsedCar(BaseRepository):
         return True
 
 
+    def delete_car(self, car_id):
+        query = 'DELETE FROM used_cars WHERE car_id = ?'
+        self.execute_query(query, (car_id,))
+        return True
+
+    def search_cars(self, search_term):
+            query = '''
+                SELECT * FROM used_cars 
+                WHERE make LIKE ? OR model LIKE ? OR year LIKE ? OR agent_id LIKE ?
+            '''
+            like_term = f'%{search_term}%'
+            return self.fetch_all(query, (like_term, like_term, like_term, like_term))
