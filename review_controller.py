a56bd919faec4e25238f3b7e3b21818ef2d8631a
zylimz107 <zylimz@hotmail.com>
2024-11-08 17:15:46 +0800

    Add updated files

--- review_controller.py
+++ review_controller.py
@@ -14,3 +14,9 @@ class SellerReviewController:
     def submit_review(self, agent_id, user_id, rating, review):
         self.review_entity.submit_review(agent_id, user_id, rating, review)
 
+class AgentReviewController:
+    def __init__(self):
+        self.review_entity = Review()
+
+    def view_reviews(self, agent_id):
+        return self.review_entity.get_reviews_by_agent(agent_id)
